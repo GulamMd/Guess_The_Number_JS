a2f003b2d4d9c8e20e5f70c66b4f9600e1009624
gulam Khalid <gulam.khalid@globalids.com>
2022-10-28 19:02:25 +0530

    Fix : Added check guessing function in script

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-
+var guessedNumber = parseInt(document.getElementsByClassName("guessNum")[0].value);
+var resultPara = document.getElementById("hint").innerHTML;
+var generatedNum = 0;
 getRandomNumber();
 function getRandomNumber() {
-    let number = Math.floor(Math.random() * 100) + 1;
-    console.log("Random generated Number: " +number);
+    this.generatedNum = Math.floor(Math.random() * 100) + 1;
+    console.log("Random generated Number: " +this.generatedNum);
+}
+
+function compareWithGuessedNum(value) {
+    if(value == generatedNum) {
+        return "Game finished";
+    }
+    else if(value > generatedNum) {
+        return `The number is less than ${value}`;
+    }
+    else {
+        return `The number is greater than ${value}`;
+    }
+}
+
+function checkGuessing() {
+    let number = parseInt(this.guessedNumber);
+    let result = this.compareWithGuessedNum(number);
+    console.log(result);
+    console.log(number);
+    console.log(this.resultPara);
+    
+    this.resultPara = result;
 }
